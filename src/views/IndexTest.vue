4ef3b9bb2bc2c6323aa44b223d9b45161a93ea67
jerry <stu89005124@yahoo.com.tw>
2023-08-08 19:18:44 +0800

    init

---
+++ src/views/IndexTest.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+	div
+		el-radio(v-model="radio" label="1") 备选项
+		el-radio(v-model="radio" label="2") 备选项2
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+				radio: "1",
+        options: [{
+          value: '选项1',
+          label: '黄金糕'
+        }, {
+          value: '选项2',
+          label: '双皮奶'
+        }, {
+          value: '选项3',
+          label: '蚵仔煎'
+        }, {
+          value: '选项4',
+          label: '龙须面'
+        }, {
+          value: '选项5',
+          label: '北京烤鸭'
+        }],
+        value: ''
+      }
+    }
+  }
+</script>
